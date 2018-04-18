c1945f8e057ac1fdad43cc3be35bb86dff758561
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-04-19 02:33:20 +1000

    Add heading

---
+++ src/components/Heading.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Heading = ({
+  value
+}) => {
+  return (
+    <div className="slds-text-heading_large slds-m-bottom--x-small">
+    { value }
+    </div>
+  )
+}
+
+export default Heading
