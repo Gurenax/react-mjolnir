0aa1a68b1d43e924f690276a69dcfd9357381b3d
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-04-19 10:20:34 +1000

    Add storybook components

--- src/stories/index.js
+++ src/stories/index.js
@@ -1,13 +1,28 @@
-import React from 'react';
+import React from 'react'
 
-import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
+import { storiesOf } from '@storybook/react'
+import { action } from '@storybook/addon-actions'
+import { linkTo } from '@storybook/addon-links'
 
-import { Button, Welcome } from '@storybook/react/demo';
+import { Button, Welcome } from '@storybook/react/demo'
 
-storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
+// Lightning Design Components
+import '../include/slds'
+import FormLayout from '../components/FormLayout'
+import Heading from '../components/Heading'
+
+storiesOf('Welcome', module).add('to Storybook', () => (
+  <Welcome showApp={linkTo('Button')} />
+))
 
 storiesOf('Button', module)
-  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
-  .add('with some emoji', () => <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>);
+  .add('with text', () => (
+    <Button onClick={action('clicked')}>Hello Button</Button>
+  ))
+  .add('with some emoji', () => (
+    <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>
+  ))
+
+storiesOf('FormLayout', module).add('default', () => <FormLayout />)
+
+storiesOf('Heading', module).add('large', () => <Heading value="Heading" />)
