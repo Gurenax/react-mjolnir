76963311a08554fbedbc3397f208c3ebe8f2d94d
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-04-19 10:23:53 +1000

    Update app imports

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,22 @@
 import React, { Component } from 'react'
 // import logo from '../assets/images/logo.svg'
 import '../styles/dist/App.css'
-import FormLayout from './FormLayout'
-import Text from './Text'
 import Heading from './Heading'
+import AbsoluteCenter from './AbsoluteCenter'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <React.StrictMode>
-          <div class="slds-align_absolute-center" style={{'height':'5rem'}}>
+          <AbsoluteCenter>
             <Heading value="React Mjolnir" />
-          </div>
-          <div class="slds-align_absolute-center" style={{'height':'5rem'}}>
+          </AbsoluteCenter>
+          <AbsoluteCenter>
             <div className="site-text-heading_label slds-m-top_none">
               Run Storybook <span className="color-background-highlight">yarn run storybook</span>
             </div>
-          </div>
+          </AbsoluteCenter>
         </React.StrictMode>
       </div>
     )
