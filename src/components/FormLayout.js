bd537117d8b9f4405f9d4104beb131109a577a15
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-04-22 23:02:47 +1000

    Update FormLayout component to use FormElement

--- src/components/FormLayout.js
+++ src/components/FormLayout.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import FormElement from './FormElement'
 
 const FormLayout = ({
   type
@@ -6,56 +7,47 @@ const FormLayout = ({
   const formClassName = 'slds-form slds-form_' + type
   return (
     <div className={formClassName}>
-      <div className="slds-form-element">
-        <label className="slds-form-element__label" htmlFor="input-id-01">Text Input</label>
-        <div className="slds-form-element__control">
-          <input type="text" id="input-id-01" className="slds-input" placeholder="Placeholder Text" />
-        </div>
-      </div>
-      <div className="slds-form-element">
-        <label className="slds-form-element__label" htmlFor="input-id-02">Textarea Label</label>
-        <div className="slds-form-element__control">
-          <textarea id="input-id-02" className="slds-textarea" placeholder="Placeholder Text"></textarea>
-        </div>
-      </div>
-      <fieldset className="slds-form-element">
-        <legend className="slds-form-element__legend slds-form-element__label">Checkbox Group label</legend>
-        <div className="slds-form-element__control">
-          <span className="slds-checkbox">
-            <input type="checkbox" name="default" id="checkbox-3" value="on" />
-            <label className="slds-checkbox__label" htmlFor="checkbox-3">
-              <span className="slds-checkbox_faux"></span>
-              <span className="slds-form-element__label">All opportunities owned by you</span>
-            </label>
-          </span>
-          <span className="slds-checkbox">
-            <input type="checkbox" name="default" id="checkbox-4" value="on" />
-            <label className="slds-checkbox__label" htmlFor="checkbox-4">
-              <span className="slds-checkbox_faux"></span>
-              <span className="slds-form-element__label">All contacts in the account owned by you</span>
-            </label>
-          </span>
-        </div>
-      </fieldset>
-      <fieldset className="slds-form-element">
-        <legend className="slds-form-element__legend slds-form-element__label">Checkbox Group label</legend>
-        <div className="slds-form-element__control">
-          <span className="slds-radio">
-            <input type="radio" id="radio-3" name="options" value="on" />
-            <label className="slds-radio__label" htmlFor="radio-3">
-              <span className="slds-radio_faux"></span>
-              <span className="slds-form-element__label">Lead Generation</span>
-            </label>
-          </span>
-          <span className="slds-radio">
-            <input type="radio" id="radio-4" name="options" value="on" />
-            <label className="slds-radio__label" htmlFor="radio-4">
-              <span className="slds-radio_faux"></span>
-              <span className="slds-form-element__label">Education Leads</span>
-            </label>
-          </span>
-        </div>
-      </fieldset>
+      <FormElement label="Text Input" forId="input-id-01">
+        <input type="text" id="input-id-01" className="slds-input" placeholder="Placeholder Text" />
+      </FormElement>
+      
+      <FormElement label="Textarea Label" forId="input-id-02">
+        <textarea id="input-id-02" className="slds-textarea" placeholder="Placeholder Text"></textarea>
+      </FormElement>
+      
+      <FormElement label="Checkbox Group label" groupControl="true">
+        <span className="slds-checkbox">
+          <input type="checkbox" name="default" id="checkbox-3" value="on" />
+          <label className="slds-checkbox__label" htmlFor="checkbox-3">
+            <span className="slds-checkbox_faux"></span>
+            <span className="slds-form-element__label">All opportunities owned by you</span>
+          </label>
+        </span>
+        <span className="slds-checkbox">
+          <input type="checkbox" name="default" id="checkbox-4" value="on" />
+          <label className="slds-checkbox__label" htmlFor="checkbox-4">
+            <span className="slds-checkbox_faux"></span>
+            <span className="slds-form-element__label">All contacts in the account owned by you</span>
+          </label>
+        </span>
+      </FormElement>
+      
+      <FormElement label="Radio Group label" groupControl="true">
+        <span className="slds-radio">
+          <input type="radio" id="radio-3" name="options" value="on" />
+          <label className="slds-radio__label" htmlFor="radio-3">
+            <span className="slds-radio_faux"></span>
+            <span className="slds-form-element__label">Lead Generation</span>
+          </label>
+        </span>
+        <span className="slds-radio">
+          <input type="radio" id="radio-4" name="options" value="on" />
+          <label className="slds-radio__label" htmlFor="radio-4">
+            <span className="slds-radio_faux"></span>
+            <span className="slds-form-element__label">Education Leads</span>
+          </label>
+        </span>
+      </FormElement>
     </div>
   )
 }
