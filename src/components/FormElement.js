776548aed317893498329b1529356567e2ca61b4
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-04-23 21:15:47 +1000

    Update FormElement component with new singleCheckbox option

--- src/components/FormElement.js
+++ src/components/FormElement.js
@@ -4,19 +4,10 @@ const FormElement = ({
   label,
   forId,
   groupControl,
+  singleCheckbox,
   children
 }) => {
-  if(!groupControl) {
-    return (
-      <div className="slds-form-element">
-        <label className="slds-form-element__label" htmlFor={ forId }>{ label }</label>
-        <div className="slds-form-element__control">
-          { children }
-        </div>
-      </div>
-    )
-  }
-  else {
+  if(!!groupControl) {
     return (
       <fieldset className="slds-form-element">
         <legend className="slds-form-element__legend slds-form-element__label">{ label }</legend>
@@ -24,6 +15,29 @@ const FormElement = ({
           { children }
         </div>
       </fieldset>
+    )
+  }
+  else if(!!singleCheckbox) {
+    return (
+      <div className="slds-form-element">
+        <span className="slds-checkbox">
+          <input type="checkbox" name="options" id={ forId } value="on" />
+          <label className="slds-checkbox__label" htmlFor={ forId }>
+            <span className="slds-checkbox_faux"></span>
+            <span className="slds-form-element__label">{ label }</span>
+          </label>
+        </span>
+      </div>
+    )
+  }
+  else {
+    return (
+      <div className="slds-form-element">
+        <label className="slds-form-element__label" htmlFor={ forId }>{ label }</label>
+        <div className="slds-form-element__control">
+          { children }
+        </div>
+      </div>
     )
   }
 }
