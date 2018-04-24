420905ec6c14fc68322f816cd7ed1235a618b0d9
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-04-24 18:31:00 +1000

    Update FormElement component to include inline help and tooltip

--- src/components/FormElement.js
+++ src/components/FormElement.js
@@ -5,15 +5,35 @@ const FormElement = ({
   forId,
   groupControl,
   singleCheckbox,
-  children
+  children,
+  inlineHelp,
+  tooltip
 }) => {
+  const useTag = "<use xlink:href=\"/icons/utility-sprite/svg/symbols.svg#info\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" />"
+
   if(!!groupControl) {
     return (
       <fieldset className="slds-form-element">
-        <legend className="slds-form-element__legend slds-form-element__label">{ label }</legend>
+        <legend className="slds-form-element__legend slds-form-element__label">{ label }
+        {
+          !!tooltip &&
+          <div className="slds-form-element__icon">
+            <button aria-describedby="help" className="slds-button slds-button_icon">
+              <svg className="slds-icon slds-icon_x-small slds-icon-text-default"
+                    aria-hidden="true"
+                    dangerouslySetInnerHTML={{__html: useTag}} />
+              <span className="slds-assistive-text">Help</span>
+            </button>
+            <div className="slds-popover slds-popover_tooltip slds-nubbin_bottom-left" role="tooltip" id="help" style={{"position": "absolute", "top": "-45px", "left": "-15px", "width": "170px"}}>
+              <div className="slds-popover__body">{ tooltip }</div>
+            </div>
+          </div>
+        }
+        </legend>
         <div className="slds-form-element__control">
           { children }
         </div>
+        { !!inlineHelp && <div className="slds-form-element__help">{ inlineHelp }</div> }
       </fieldset>
     )
   }
@@ -26,6 +46,21 @@ const FormElement = ({
             <span className="slds-checkbox_faux"></span>
             <span className="slds-form-element__label">{ label }</span>
           </label>
+          {
+            !!tooltip &&
+            <div className="slds-form-element__icon">
+              <button aria-describedby="help" className="slds-button slds-button_icon">
+                <svg className="slds-icon slds-icon_x-small slds-icon-text-default"
+                      aria-hidden="true"
+                      dangerouslySetInnerHTML={{__html: useTag}} />
+                <span className="slds-assistive-text">Help</span>
+              </button>
+              <div className="slds-popover slds-popover_tooltip slds-nubbin_bottom-left" role="tooltip" id="help" style={{"position": "absolute", "top": "-45px", "left": "-15px", "width": "170px"}}>
+                <div className="slds-popover__body">{ tooltip }</div>
+              </div>
+            </div>
+          }
+          { !!inlineHelp && <div className="slds-form-element__help">{ inlineHelp }</div> }
         </span>
       </div>
     )
@@ -34,10 +69,26 @@ const FormElement = ({
     return (
       <div className="slds-form-element">
         <label className="slds-form-element__label" htmlFor={ forId }>{ label }</label>
+        {
+          !!tooltip &&
+          <div className="slds-form-element__icon">
+            <button aria-describedby="help" className="slds-button slds-button_icon">
+              <svg className="slds-icon slds-icon_x-small slds-icon-text-default"
+                    aria-hidden="true"
+                    dangerouslySetInnerHTML={{__html: useTag}} />
+              <span className="slds-assistive-text">Help</span>
+            </button>
+            <div className="slds-popover slds-popover_tooltip slds-nubbin_bottom-left" role="tooltip" id="help" style={{"position": "absolute", "top": "-45px", "left": "-15px", "width": "170px"}}>
+              <div className="slds-popover__body">{ tooltip }</div>
+            </div>
+          </div>
+        }
         <div className="slds-form-element__control">
           { children }
         </div>
+        { !!inlineHelp && <div className="slds-form-element__help">{ inlineHelp }</div> }
       </div>
+
     )
   }
 }
